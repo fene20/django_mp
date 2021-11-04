bc41854d91f537d356d913021bc4164f619fbfd5
fene20 <feeneyk@gmail.com>
2021-11-04 18:34:32 +0000

    add graphic views and templates

--- graphics/views.py
+++ graphics/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Graphic
 
 # Create your views here.
+
+def all_graphics(request):
+    """ A view to show all graphics, including sorting and search queries """
+
+    graphics = Graphic.objects.all()
+
+    context = {
+        'graphics': graphics,
+    }
+
+    return render(request, 'graphics/graphics.html', context)
--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 # import from current directory
 from . import views
