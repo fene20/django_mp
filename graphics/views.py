ce53878f038e9daa6dfff61f8f2773a8af9fecd0
fene20 <feeneyk@gmail.com>
2021-11-04 21:17:35 +0000

    add search functionality

--- graphics/views.py
+++ graphics/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Graphic
 
 # Create your views here.
@@ -7,9 +9,23 @@ def all_graphics(request):
     """ A view to show all graphics, including sorting and search queries """
 
     graphics = Graphic.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('graphics'))
+
+            # force an OR of queries otherwise search will look for AND of queries
+            # | for OR and i in icontains for case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            graphics = graphics.filter(queries)
 
     context = {
         'graphics': graphics,
+        'search_term': query,
     }
 
     return render(request, 'graphics/graphics.html', context)
