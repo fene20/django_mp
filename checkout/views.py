8379e66f9912021aded691acb908a6a401dc2e8d
fene20 <feeneyk@gmail.com>
2021-11-17 00:23:16 +0000

    add cstripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jj2kiHUE1qNx3daPSjMWFy8nmnW5FCB7ywFoBsnUi9dLRywzydfUK9JLOLEjJfPIT6CzYWOyzPuzSQEnglnEeKW002FkVkSCQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
