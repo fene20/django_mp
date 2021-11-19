824a2d43e93b7fe61a9b1582d43dcbfaf5042110
fene20 <feeneyk@gmail.com>
2021-11-19 23:29:55 +0000

    graphic admin - fix image field part 1

--- graphics/forms.py
+++ graphics/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Graphic, Category
 
 
@@ -8,6 +9,8 @@ class GraphicForm(forms.ModelForm):
         model = Graphic
         # dunder = double underscore
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
