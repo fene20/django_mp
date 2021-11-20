74f703fe8403dfec20d3aea6c1b72a49f029e4fd
fene20 <feeneyk@gmail.com>
2021-11-20 19:10:25 +0000

    sfix bucket name spelling andtry adeployment again

--- great_graphics/settings.py
+++ great_graphics/settings.py
@@ -196,7 +196,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'great-grapics'
+    AWS_STORAGE_BUCKET_NAME = 'great-graphics'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
