d9962146826296ffb18ccdbd589679a4aaef17c1
fene20 <feeneyk@gmail.com>
2021-11-20 15:48:13 +0000

    deployment part 2

--- great_graphics/settings.py
+++ great_graphics/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--el_r5#a&2!h)32jvo93^zed&m000x8rliyv%)n^(%kn64(0jp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['great-graphics.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,19 @@ WSGI_APPLICATION = 'great_graphics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
